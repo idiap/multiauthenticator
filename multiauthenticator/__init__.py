2cd99801b3cc0231c01afd5e4fa7cf64e6c03fe9
Samuel Gaist <samuel.gaist@idiap.ch>
2023-11-20 15:43:46 +0100

    Bump to 0.3.0.dev

--- multiauthenticator/__init__.py
+++ multiauthenticator/__init__.py
@@ -7,4 +7,4 @@ from multiauthenticator.multiauthenticator import MultiAuthenticator  # noqa
 # __version__ should be updated using tbump, based on configuration in
 # pyproject.toml.
 #
-__version__ = "0.2.0"
+__version__ = "0.3.0.dev"
